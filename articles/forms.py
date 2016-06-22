af8f9a7177cf3357c3652e7227558dc2d77f37a4
xiaoxuncle <open_source@126.com>
2016-06-22 22:27:47 +0800

    nothing

--- articles/forms.py
+++ articles/forms.py
@@ -7,7 +7,7 @@ from articles.models import Article, Node
 class ArticleForm(forms.Form):
     node_choices = []
     node_names = Node().node_names()
-    for name in list(node_names):
+    for name in node_names:
     	node_choices.append((name, name))
     title = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'请输入主题标题'}), max_length=255)
     content = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control'}), max_length=4000)
